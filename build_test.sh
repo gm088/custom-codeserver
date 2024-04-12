93835fd7a19810b4ecb307c5ab6d220d81424894
Gaurav Mandana <53940031+gm088@users.noreply.github.com>
2024-04-12 16:05:18 +0200

    Update build_test.sh

--- build_test.sh
+++ build_test.sh
@@ -3,7 +3,3 @@ IMAGE_VERSION=$(date '+%d%m%y')
 docker build -t gm088/cserver:${IMAGE_VERSION} -t gm088/cserver:latest . && \
 docker push gm088/cserver:${IMAGE_VERSION}
 docker push gm088/cserver:latest
-
-
-sudo docker build -t gm088/cserver:${IMAGE_VERSION} . && \
-sudo docker push gm088/cserver:${IMAGE_VERSION}
