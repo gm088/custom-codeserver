bd50c7bdd6c1943dcf42dc459cff5247bd12ff7f
Gaurav Mandana <53940031+gm088@users.noreply.github.com>
2024-04-18 11:57:16 +0200

    check for pip conf file

--- vscoderun.sh
+++ vscoderun.sh
@@ -60,8 +60,8 @@ export SINGULARITYENV_PYTHONPATH="${packages_install_path}:/usr/lib/python310.zi
 # RUN CODE SERVER FROM SINGULARITY
 #-------------------------------------------------------------------------
 
-####NB for code server have to manually set 0.0.0.0 to listen at all interfaces
-#### otherwise it uses a particular interface
+####NB for code server have set 0.0.0.0 to listen at all interfaces
+#### otherwise it uses localhost
 #### pass the password as an environment variable according to code-server --help
 
 /hpcnfs/software/singularity/3.7.0/bin/singularity exec --cleanenv \
